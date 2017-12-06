efcdb91a81b215f26038043ffff18dd352cded11
Robi Parvez <parvezrobi@yahoo.com>
2017-12-06 13:04:33 +0600

    Added 'crud-dec-6' project

---
+++ app/Http/Controllers/ToperatorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Toperator;
+use Illuminate\Http\Request;
+
+class ToperatorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Toperator  $toperator
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Toperator $toperator)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Toperator  $toperator
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Toperator $toperator)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Toperator  $toperator
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Toperator $toperator)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Toperator  $toperator
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Toperator $toperator)
+    {
+        //
+    }
+}
